34be593259da30328fdca618b5838237c56ac8bf
Smartisek <domalipdominik@gmail.com>
2024-11-15 17:38:34 +0000

    :pencil2: Adding explanation for remove function
    Added explanation of the complicated remove function for my own clear understanding how it works, also adding a link to a video showing visual working of this function.

--- ADS_2024_CA2_Dominik_Domalip/BinaryTree.h
+++ ADS_2024_CA2_Dominik_Domalip/BinaryTree.h
@@ -80,82 +80,96 @@ int BinaryTree<K, V>::count()
 template <class K, class V>
 bool BinaryTree<K, V>::remove(K& key)
 {
-	BSTNode<T>* toBeRemoved = root;
-	BSTNode<T>* parent = nullptr;
-	bool found = false;
+	/******Visualisation for this alghorithm: https://www.youtube.com/watch?v=DkOswl0k7s4 ******/
+	/*Help Variables*/
+	BSTNode<K, V>* toBeRemoved = root; //points to the node needed to remove
+	BSTNode<K, V>* parent = nullptr; //will point to the parent of toBeRemoved, keeps track when traversing tree
+	bool found = false; //check if we found needed node
 
-	while (!found && toBeRemoved != nullptr)
+	while (!found && toBeRemoved != nullptr) //when node is not yet found and root is not empty
 	{
 
-		if (toBeRemoved->getKey() == key)
+		if (toBeRemoved->getKey() == key) //check for when the key is actually the first node which is root 
 		{
 
-			found = true;
+			found = true; //was found
 		}
 		else
 		{
-			parent = toBeRemoved;
-			if (toBeRemoved->getKey() > key)
+			parent = toBeRemoved; //set the parent to the current node
+			if (toBeRemoved->getKey() > key) //check if the current node's key is more than what we are looking for
 			{
-				toBeRemoved = toBeRemoved->getLeft();
+				toBeRemoved = toBeRemoved->getLeft(); //when yes, move to the left child and parent is the previous node
 			}
 			else
 			{
-				toBeRemoved = toBeRemoved->getRight();
+				toBeRemoved = toBeRemoved->getRight(); //when key is less than the current node, move to the right child
 			}
 		}
-	}
+	} //exits while loop when we found node we are loking for
+
+	//node is not present in this tree
 	if (!found)
 		return false;
 
-	if (toBeRemoved->getLeft() == nullptr || toBeRemoved->getRight() == nullptr)
+	/***Remove when node has max one child***/
+	if (toBeRemoved->getLeft() == nullptr || toBeRemoved->getRight() == nullptr) //check if node has max one child
 	{
-		BSTNode<K, V>* newChild;
-		if (toBeRemoved->getLeft() == nullptr)
+		/*Deciding on newChild*/
+		BSTNode<K, V>* newChild; //new pointer will point to the child of the node to be removed
+		if (toBeRemoved->getLeft() == nullptr) //if the node to be removed does not have left kid
 		{
-			newChild = toBeRemoved->getRight();
+			newChild = toBeRemoved->getRight(); //set the new child to right child
 		}
-		else
+		else //node to be removed does not have right child
 		{
-			newChild = toBeRemoved->getLeft();
+			newChild = toBeRemoved->getLeft(); //set the new child to left child
 		}
-		if (parent == nullptr)
+
+		/*Giving a newChild to a parent instead of toBeRemoved node*/
+		if (parent == nullptr) //when node to be removed is the root 
 		{
-			root = newChild;
+			root = newChild; //update the root to be the new child 
 		}
-		else if (parent->getLeft() == toBeRemoved)
+		else if (parent->getLeft() == toBeRemoved) //when parents left child is what we are looking for
 		{
-			parent->setLeft(newChild);
+			parent->setLeft(newChild); //change the left child from toBeRemoved to newChild 
 		}
-		else
+		else //parents right child is the node needed to be removed 
 		{
-			parent->setRight(newChild);
+			parent->setRight(newChild); //set the right child to be the new child
 		}
-		return true;
+		delete toBeRemoved; //free the memory when not needed anymore
+		return true; //return true when node was removed
 	}
 
-	BSTNode<K, V>* smallestParent = toBeRemoved;
-	BSTNode<K, V>* smallest = toBeRemoved->getRight();
+	/*Logic for removing node with TWO CHILDREN*/
+	/*Another way could be to find the biggest node in left subtree*/
+	BSTNode<K, V>* smallestParent = toBeRemoved; //will keep track of the parent of the smallest node
+	BSTNode<K, V>* smallest = toBeRemoved->getRight(); //starts as right child of node to be removed
+	//find the smallest node in the right subtree
 	while (smallest->getLeft() != nullptr)
 	{
-		smallestParent = smallest;
-		smallest = smallest->getLeft();
+		smallestParent = smallest; //set the parent of the smallest node to be the current smallest node
+		smallest = smallest->getLeft();//found the smallest node in the right subtree
 	}
-	toBeRemoved->setKey(smallest->getKey());
-	toBeRemoved->setValue(smallest->getValue());
+	toBeRemoved->setKey(smallest->getKey()); //change toBeRemoved node key to be the smallest one in right subtree
+	toBeRemoved->setValue(smallest->getValue());  //change toBeRemoved node value to be the smallest one in right subtree
 
-	if (smallestParent == toBeRemoved)
+	if (smallestParent == toBeRemoved) //when smallestParent is same node as to be removed
 	{
-		smallestParent->setRight(smallest->getRight());
+		smallestParent->setRight(smallest->getRight()); //right child of smallest parent is now the right child of smallest
 	}
-	else
+	else //when smallestParent is not the same as toBeRemoved
 	{
-		smallestParent->setLeft(smallest->getRight());
+		smallestParent->setLeft(smallest->getRight()); //left child of smallest parent is now the right child of smallest
 	}
 
-	delete smallest;
+	delete smallest; //delete the smallest node
+	return true; //return true when node was removed
 
 }
+
 template <class K, class V>
 V& BinaryTree<K, V>::get(K& key)
 {
@@ -214,13 +228,9 @@ void BinaryTree<K, V>::clear(BSTNode<K, V>* node) //private helper function for 
 }
 
 template <class K, class V>
-BinaryTree<K, V>::~BinaryTree()
+BinaryTree<K, V>::~BinaryTree() //Destructor Function for memory management
 {
-	if (root != nullptr)
-	{
-		delete root;
-		root = nullptr;
-	}
+	clear(); //call the clear function to delete all nodes and set root to null
 }
 
 template <class K, class V>
